ef7b671dfcd7e4bd59e63b207331b31344ebc51b
Steven Dorsher <sdorsher@gmail.com>
2019-02-06 17:02:02 -0600

    need to look up Energy Momentum relationship for Newtonian mechanics. Have textbook beside me.

--- manybodysimple.py
+++ manybodysimple.py
@@ -32,9 +32,11 @@ def InitialData():
     size2=50
     size3=10000
     #these are guesses from memory-- correct values will need to be filled in later or the code will need to be adapted to open or closed clusters
+    masscategory0=np.ones(1);
     masscategory1=0.001*np.random.uniform(.9,1.1,size1); #Msun (jupiter)
     masscategory2=0.00001*np.random.uniform(0.2*5,size2); #Msun (Earth)
     masscategory3=0.0000001*np.random.uniform(0.01,100,size3); #comet
+
     #initially use an in plane orbit with random starting locations relative to the x axis
     phi0category1=np.random.uniform(0,1,size1);
     phi0category2=np.random.uniform(0,1,size2);
@@ -45,6 +47,14 @@ def InitialData():
     orbitalradius1=np.random.uniform(2.,50.,size1);
     orbitalradius2=np.random.uniform(.1,2,size2);
     orbitalradius3=np.random.uniform(.1,50,size3);
+    eccentricity1=np.random.uniform(0.,.1,size1);
+    eccentricity2=np.random.uniform(0.,.1,size2);
+    eccentricity3=np.random.uniform(0,1.,size3);
+    
+
+
+    x00=np.zeros(1)
+    y00=np.zeros(1)
     x01=orbitalradius1*np.cos(phi0category1);
     y01=orbitalradius1*np.sin(phi0category1);
     x02=orbitalradius1*np.cos(phi0category2);
@@ -52,14 +62,40 @@ def InitialData():
     x03=orbitalradius1*np.cos(phi0category3);
     y03=orbitalradius1*np.sin(phi0category3);
     
-    x0=np.zeros(size1+size2+size3);
-    y0=np.zeros(size1+size2+size3);
-    x0[1:size1]=x01;
-    x0[size1:size1+size2]=x02;
-    x0[size2:size1+size2+size3]=x03;
-    y0[1:size1]=y01;
-    y0[size1:size1+size2]=y02;
-    y0[size2:size1+size2+size3]=y03;
+    x0=np.zeros(1+size1+size2+size3);
+    y0=np.zeros(1+size1+size2+size3);
+    masses=np.zero(1+size1+size2+size3);
+    x0[0]=0.0;
+    x0[2:1+size1]=x01;
+    x0[1+size1:1+size1+size2]=x02;
+    x0[1+size1+size2:1+size1+size2+size3]=x03;
+    y0[0]=0.0;
+    y0[2:1+size1]=y01;
+    y0[1+size1:1+size1+size2]=y02;
+    y0[1+size1+size2:1+size1+size2+size3]=y03;
+    masses[0]=1.0;
+    masses[2:1+size1]=masscategory1;
+    massess[1+size1:1+size1+size2]=masscategory2;
+    masses[1+size1+size2:1+size1+size2+size3]=masscategory3;
+    ecc[0]=0.0;
+    ecc[2:1+size1]=eccentricity1;
+    ecc[1+size1:1+size1+size2]=eccentricity2;
+    ecc[1+size1+size2:1+size1+size2+size3]=eccentricity3;
+    
+
     
 
     #velocity initial conditions are not trivial. 
+    #I need a sun. Treat everything as a perturbation on a self consistent classical energy and momentum background. 
+
+
+
+    
+
+
+    #there is a units problem that needs to be fixed
+    
+    
+
+    return masses,x0,y0, ux0, uy0;;
+
