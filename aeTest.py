3a3f92b14567505d61de263031db12d6a03ee9a2
Steven Dorsher <sdorsher@gmail.com>
2020-05-04 19:41:39 -0500

    Added two plots related to symmetry about the initial orientation. Near aphelion the symmetry of the orbit is good, but it is difficult to tell near perhelion where the orbit moves so fast it is difficult to get good cancellation between neighboring points. I have not yet tried something fancy like parabolic interpolation.

--- aeTest.py
+++ aeTest.py
@@ -1,3 +1,4 @@
+import numpy as np
 def localMinimumIndex(values):
     mins=[]
     secondtolastval=values[0]
@@ -12,15 +13,15 @@ def localMinimumIndex(values):
 
 def findPerApHelion(npstar1x,theta0,ecc0,rad0):
     b=np.tan(theta0)
-    x0,y0=npstar1x[0,0],npstar1y[0,1]
+    x0,y0=npstar1x[0,0],npstar1x[0,1]
     mostb = []
     mostb1 = np.abs(1-npstar1x[:,1]/npstar1x[:,0]/b)
-    mostb2=np.abs(1-b/npstarx1[:,1]*npstar[:,0])
+    mostb2=np.abs(1-b/npstar1x[:,1]*npstar1x[:,0])
     for b1,b2 in zip(mostb1,mostb2):
         mostb.append(min(b1,b2))
     indexminmostb=np.argmin(mostb) #aphelion
-    indexminmostb2=np.argmin(mostb[20:20+indexminmostb-20]) #perihelion
-    assert np.abs(npstarx1[0,0]-npstar1[indexminmostb,0])<1, "did not find aphelion"
+    indexminmostb2=np.argmin(mostb[20:20+indexminmostb-20])+20 #perihelion
+    assert np.abs(npstar1x[0,0]-npstar1x[indexminmostb,0])<1, "did not find aphelion"
     assert np.abs(indexminmostb2/indexminmostb-1)<0.05, "did not find perihelion"
     mins=localMinimumIndex(mostb[0:indexminmostb2+1])
     coordper=[npstar1x[mins[0],0],npstar1x[mins[0],1]]
@@ -39,4 +40,6 @@ def findPerApHelion(npstar1x,theta0,ecc0,rad0):
     deltae=np.abs(e-ecc0)/ecc0
     print("delta a", deltaa)
     print("delta e", deltae)
-    return coordper,coordap,rp,ra,e,a,deltae,deltaa
+    print("perhelion index", mins[0])
+    print("aphelion index", indexminmostb2)
+    return mins[0],indexminmostb2,coordper,coordap,rp,ra,e,a,deltae,deltaa
