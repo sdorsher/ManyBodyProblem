97dced8dd18546ed9faadc9e76361bf27f83b943
Steven Dorsher <sdorsher@gmail.com>
2020-08-03 18:33:18 -0500

    Fixed a bug in the adaptive step size-- but it still inspirals

--- RK4adaptive.py
+++ RK4adaptive.py
@@ -7,11 +7,12 @@ def RK4adaptive(h,t,xvec,f,delta):
     rho=30*h*delta/np.mean(np.abs(x1-x2))
     repeat=False
     hnew=h
-    if rho>1:
+    epsilon=1e-6
+    if np.abs(np.abs(rho)-1)<epsilon:
         repeat=False
-    elif rho<1:
+    elif np.abs(rho)>1:
+        repeat=False
+    elif np.abs(rho)<1:
         repeat=True
-    elif rho==1:
-        repeat=False
     hnew*=rho**.25
     return repeat, hnew, h, t1, x1, rho
